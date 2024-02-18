56891257d0d82c632a845b4f435a30088d6fce4c
saisreesatyassss <saisreesatyassss@gmail.com>
2024-02-18 14:14:19 +0530

    intial framework

---
+++ src/app/Pages/Main/pages.tsx
@@ -0,0 +1,17 @@
+// Import the necessary libraries
+"use client";
+import React, { useState } from 'react';  
+ 
+ 
+  function Main() {
+
+  return (
+    // Main section with a full height background
+    <section className=" h-screen relative">
+    <h1>this main page</h1>
+    </section>
+  );
+}
+
+// Export the Main component
+export default Main;
